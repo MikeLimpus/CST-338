d322827c1782c2e90f3c9318c142798b2bc957a8
Mike Limpus <mikelimpus111@gmail.com>
2021-03-04 20:21:55 -0800

    Finished first lab

--- ProgrammingLabs/Module1/StringManipulation.java
+++ ProgrammingLabs/Module1/StringManipulation.java
@@ -2,11 +2,17 @@
  * Mike Limpus
  * CST 338
  * Lab 1 - String Manipulation
+ * Part 1: Get the user's name and output the length and the name in upper and 
+ * lower case.
+ * Part 2: Get the number of hours the user worked on class and output the 
+ * value rounded to a tenth.
  */
 import java.util.Scanner;
-
+import java.text.DecimalFormat;
 public class StringManipulation {
     public static void main(String[] args) {
+        /* The two parts of the lab are seperated into different methods for 
+        the sake ease of evaluating them seperating them */
         manipulateName();
         classHours();
     }
@@ -17,6 +23,7 @@ public class StringManipulation {
         String firstName; 
         String lastName;
         Scanner input = new Scanner(System.in);
+        System.out.println("Part 1");
         System.out.println("Please enter your first and last name,"
             + "capitalized properly \ne.g. John Doe");
         // User will input their first and last name, seperately
@@ -32,17 +39,30 @@ public class StringManipulation {
         // Print the name as defined by the assignment 
         System.out.println("\nName: " + fullName + "\nLength: " + nameLength);
         System.out.println("Upper case: " + upperCase
-            + "\nLower case: " + lowerCase);
-        input.close();
-
+            + "\nLower case: " + lowerCase + "\n");
     }
 
     // Part 2
+    // These are the global constants 
     public static final int MIN_HOURS = 12;
     public static final int MAX_HOURS = 20;
 
     public static void classHours() {
-        
-
+        Scanner input = new Scanner(System.in);
+        DecimalFormat formatter = new DecimalFormat("00.0");
+        double hoursWorked;
+        String roundedHours;
+        System.out.println("Part 2");
+        System.out.println("You should work anywhere between " + MIN_HOURS 
+            + " and " + MAX_HOURS + " week in this course.\nTo three decimal" 
+            + " places, how many hours did you work?");
+        // Get the value from the user and round to one dec. place
+        System.out.print("Number of hours: " );
+        hoursWorked = input.nextDouble();
+        roundedHours = formatter.format(hoursWorked);
+        // For the purpose of testing printf vs println as discussed in the text
+        System.out.printf("\nYou worked %s hours this week\n", roundedHours);
+        // Close input to prevent resource leak
+        input.close();
     }
 }
