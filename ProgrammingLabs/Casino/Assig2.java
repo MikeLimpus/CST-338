3b6cbb6ed8110e47fc9ec0f4ed8784d6f381af76
Mike Limpus <mikelimpus111@gmail.com>
2021-03-10 11:46:09 -0800

    Lab 2 w/ first documentation

---
+++ ProgrammingLabs/Casino/Assig2.java
@@ -0,0 +1,31 @@
+/**
+ * Jonathan Limpus 
+ * CST 338
+ * 3/10/2021
+ * 
+ * Module 2: Casino
+ * From the Assignment Page: "The program will loop, asking the user for a bet 
+ * amount from 0 to 100 (assume dollars, you can use ints or longs).  If the 
+ * user types a 0 that means she wants to quit.  Otherwise, accept the amount as
+ * their bet and simulate a slot machine pull.  Your program will print out a 
+ * line that looks like a slot machine result containing three strings."
+ */
+
+import java.util.*;
+import java.lang.Math;
+
+public class Assig2 {
+    
+}
+
+/**
+ * Sibling class to support the main program.
+ * Contains three private member Strings, public static final int MAX_LEN 
+ * and MAX_PULLS, static final int array pullWinnings
+ * and an int numPulls. Public methods: Default constructor
+ * ThreeString(), accessors and boolean mutators, and toString() boolean 
+ * saveWinnings(int winnings), and String toStringWinnings() methods.
+ */
+class ThreeString {
+
+}
